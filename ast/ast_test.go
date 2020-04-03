61a86d25f6863efbfaaad69e3678ba280817e3ff
JuanPablo Ramos <jprscs@gmail.com>
2020-04-03 12:19:05 -0600

    Creates ast package

---
+++ ast/ast_test.go
@@ -0,0 +1,31 @@
+package ast_test
+
+import (
+	"testing"
+
+	"github.com/jpr98/wiig/ast"
+
+	"github.com/jpr98/wiig/token"
+)
+
+func TestString(t *testing.T) {
+	program := &ast.Program{
+		Statements: []ast.Statement{
+			&ast.LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &ast.Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "myVar"},
+					Value: "myVar",
+				},
+				Value: &ast.Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+
+	if program.String() != "let myVar = anotherVar;" {
+		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
+}
